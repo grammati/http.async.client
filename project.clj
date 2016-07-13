11590e92e4945e01759daeafe2331e48423c65c7
Chris Perkins <chrisperkins99@gmail.com>
2016-07-13 08:01:55 -0600

    Bump clojure dependency to 1.8.0
    This required a bump of aleph to 0.4.1 too, because 0.4.0 does not work
    with clojure 1.8

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject http.async.client "1.1.0"
+(defproject http.async.client "1.2.0-SNAPSHOT"
   :name             "http.async.client"
   :description      "Asynchronous HTTP Client for Clojure"
   :url              "https://github.com/cch1/http.async.client"
   :source-paths ["src/clj"]
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.ning/async-http-client "1.9.31"]
                  [org.clojure/tools.logging "0.3.1"]]
   :min-lein-version "2.5.1"
@@ -13,11 +13,12 @@
              :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6.0 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7.0 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :dev {:resource-paths ["test-resources"],
-                   :dependencies [[org.eclipse.jetty/jetty-server "9.3.7.RC0"]
+                   :dependencies [[org.eclipse.jetty/jetty-server "9.3.8.RC0"]
                                   [org.eclipse.jetty/jetty-security "9.3.7.RC0"]
                                   [org.eclipse.jetty/jetty-continuation "9.3.7.RC0"]
-                                  [aleph "0.4.0"]
+                                  [aleph "0.4.1"]
                                   [log4j "1.2.17"]
                                   [org.slf4j/slf4j-log4j12 "1.7.13"]]}}
   ;; :repositories {"snapshots" "http://oss.sonatype.org/content/repositories/snapshots/"}
